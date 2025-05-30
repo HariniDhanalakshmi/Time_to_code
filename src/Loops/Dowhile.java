e4e9588832eae6096a0d247a7e9b6200844e4c26
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-05-30 16:42:01 +0530

    Day 4 files

---
+++ src/Loops/Dowhile.java
@@ -0,0 +1,18 @@
+package Loops;
+import java.util.Scanner;
+public class Dowhile {
+    public static void main(String[] args) {
+        int a = 1;
+        do{
+            System.out.println(a);
+            a++;
+        }while(a < 5);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter b: ");
+        int b = input.nextInt();
+        do {
+            System.out.print(b+ " ");
+            b++;
+        } while(b<5);
+        }
+    }

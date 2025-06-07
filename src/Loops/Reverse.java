f1f21a7f5f96c883f765bcb0ad8b4a603a54332e
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-06-07 09:27:40 +0530

    Day 6 files

---
+++ src/Loops/Reverse.java
@@ -0,0 +1,14 @@
+package Loops;
+
+public class Reverse {
+    public static void main(String[] args) {
+        int num = 34390;
+        int ans = 0;
+        while (num > 0) {
+            int rem = num % 10;
+            num /= 10;
+            ans = ans * 10 + rem;
+        }
+        System.out.println(ans);
+    }
+}

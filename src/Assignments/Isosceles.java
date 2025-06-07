02b405d0708f2548efd760a8e6a6bf8b4cd4aedc
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-06-07 14:24:39 +0530

    Day 7 file

---
+++ src/Assignments/Isosceles.java
@@ -0,0 +1,16 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class Isosceles {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //Area of Isosceles Triangle = 0.5*b*h
+        System.out.print("Enter the value of breadth: ");
+        float breadth = input.nextFloat();
+        System.out.print("Enter the value of height: ");
+        float height = input.nextFloat();
+        float area = (float)0.5 * breadth * height;
+        System.out.println("Area of Isosceles Triangle: " +area);
+    }
+}

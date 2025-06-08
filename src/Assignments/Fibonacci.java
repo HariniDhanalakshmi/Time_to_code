312b6ec8e035e6cbd1610772d2b9057fdbcca7b6
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-06-08 09:43:06 +0530

    Day 8 file

---
+++ src/Assignments/Fibonacci.java
@@ -0,0 +1,21 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //Fibonacci Series In Java Programs
+        System.out.print("Enter the number of terms (n): ");
+        int n = in.nextInt();
+        int first = 0, second = 1;
+        System.out.println("Fibonacci series up to " + n + " terms:");
+        for(int i =1; i<=n; i++){
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
+

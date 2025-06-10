758620bfdee1342f489cfe4ef807051a6d163c45
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-06-10 17:17:53 +0530

    Day 9 file

---
+++ src/Methods/Example2.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class Example2  {
+    public static void main(String[] args) {
+        String stmt = greets();
+        System.out.println(stmt);
+    }
+    static String greets(){
+        String p = "How are you!";
+        return p;
+    }
+}

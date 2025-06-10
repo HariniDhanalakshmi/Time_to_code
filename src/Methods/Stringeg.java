021eac544bad7b05a941bb9d1c635bf1b0891053
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-06-10 17:03:06 +0530

    Day 9 file

---
+++ src/Methods/Stringeg.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class Stringeg {
+    public static void main(String[] args) {
+        String stmt = greet();
+        System.out.println(stmt);
+    }
+    static String greet(){
+        String greeting = "How are you!";
+        return greeting;
+    }
+}

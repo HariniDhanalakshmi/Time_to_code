5cb297d044b70a86a8c18277c894c967cb3937c8
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-06-10 17:19:22 +0530

    Day 9 file

---
+++ src/Methods/Example.java
@@ -0,0 +1,11 @@
+package Methods;
+public class Example {
+    public static void main(String[] args) {
+        int aa = greeter();
+        System.out.println(aa);
+    }
+    static Integer greeter() {
+        int numb = 54;
+        return numb;
+    }
+}

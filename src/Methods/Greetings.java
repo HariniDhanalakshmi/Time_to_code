1decd0c69ec22fde5df23622cf873c346aec92c3
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-06-10 16:24:45 +0530

    Day 9 file

---
+++ src/Methods/Greetings.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class Greetings {
+    public static void main(String[] args) {
+        Greetings();
+        Greetings();
+    }
+    static void Greetings(){
+        System.out.println("This is the first program in methods!");
+    }
+}
+

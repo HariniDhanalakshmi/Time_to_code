b7a54ef59dcb2ee6a52610fb5287362838b80ff1
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-06-12 11:42:55 +0530

    Day 10 file

---
+++ src/Methods/Exam2.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class Exam2 {
+    public static void main(String[] args) {
+        String ouput = great("Harini");
+        System.out.println(ouput);
+    }
+    static String great(String name){
+        String out = "Hello " + name;
+        return out;
+    }
+}

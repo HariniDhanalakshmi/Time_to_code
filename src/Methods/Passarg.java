95d8a382cccfbcf6500c56c3e6e6e9534d9f506b
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-06-12 11:31:36 +0530

    Day 10 file

---
+++ src/Methods/Passarg.java
@@ -0,0 +1,14 @@
+package Methods;
+
+public class Passarg {
+    public static void main(String[] args) {
+        int ans = sum(233,32);
+        System.out.println(ans);
+
+    }
+    static int sum(int p,int q){
+        int addition = p+q;
+        return addition;
+    }
+
+}
